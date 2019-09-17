6504067bf21eecb0b28915a5553d1b42156b2b03
HanKruiger <hankruiger@gmail.com>
2019-09-17 11:15:30 +0200

    New data format, and add shorthand notation of semantically encoded question.

--- parse.py
+++ parse.py
@@ -3,11 +3,15 @@
 import json
 import re
 
+
+def question_sequence_to_string(question_sequence):
+    return "".join([f"[{token['tag']}:{token['value']}]" for token in question_sequence])
+
 if __name__ == '__main__':
     with open('./analyzed_question.json') as f:
         questions = json.load(f)
-        expression = '1.*a.*' # matches a where question with an activity.
+        expression = '5.*' # matches a where question with an activity.
         matcher = re.compile(expression)
         for q in questions:
-            if matcher.match(q['code']):
-                print(f"Question {q['id']} matched: {q['question']}?")
+            q['shorthand'] = question_sequence_to_string(q['all_info'])
+            print(q['shorthand'])
