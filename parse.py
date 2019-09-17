bfc4e67f97a1e722ad9f6ca60e2b12507a4867aa
HanKruiger <hankruiger@gmail.com>
2019-09-17 12:57:47 +0200

    Generalized the separator and brackets so we can easily change.

--- parse.py
+++ parse.py
@@ -4,15 +4,14 @@ import json
 import re
 
 
-def question_sequence_to_string(question_sequence):
+def question_sequence_to_string(question_sequence, separator=':', brackets=('[', ']')):
     """Returns the shorthand notation of the semantically encoded question."""
-    return "".join([f"[{token['tag']}:{token['value']}]" for token in question_sequence])
+    return "".join([f"{brackets[0]}{token['tag']}{separator}{token['value']}{brackets[1]}" for token in question_sequence])
+
 
 if __name__ == '__main__':
     with open('./analyzed_question.json') as f:
         questions = json.load(f)
-        expression = '5.*' # matches a where question with an activity.
-        matcher = re.compile(expression)
         for q in questions:
             q['shorthand'] = question_sequence_to_string(q['all_info'])
             print(q['shorthand'])
